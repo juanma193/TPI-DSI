fa22b79b6682f4db72f90632f41b934d308d292a
Diego Sosa <diegososa015@gmail.com>
2023-06-01 10:35:36 -0300

    Update BaseDeDatos.cs
    Cambio de nombres de BD

--- TPIDSI/BaseDeDatos.cs
+++ TPIDSI/BaseDeDatos.cs
@@ -18,9 +18,9 @@ namespace TPIDSI
 
         //Creamos accion
         public static Accion accion1 = new Accion("Dar de baja a la tarjeta");
-        public static Accion accion2 = new Accion("Mover 2 pasos");
-        public static Accion accion3 = new Accion("Ir al gimnasio");
-        public static Accion accion4 = new Accion("Aprobar directo diseño");
+        public static Accion accion2 = new Accion("Pedir nueva tarjeta");
+        public static Accion accion3 = new Accion("Pausar acciones de la tarjeta");
+        public static Accion accion4 = new Accion("Otros motivos");
         public static List<Accion> listaAcciones = new List<Accion>() { accion1, accion2, accion3, accion4};
 
 
@@ -35,9 +35,9 @@ namespace TPIDSI
         public static List<OpcionValidacion> validacionList = new List<OpcionValidacion>() { opcionValidacion1, opcionValidacion2, opcionValidacion3 };
 
         //Creamos validaciones
-        public static Validacion validacion1 = new Validacion("validacion1","hola", 1, validacionList);
-        public static Validacion validacion2 = new Validacion("validacion2","estamos", 2, validacionList);
-        public static Validacion validacion3 = new Validacion("validacion3","listos", 3, validacionList);
+        public static Validacion validacion1 = new Validacion("validacion 1","hola", 1, validacionList);
+        public static Validacion validacion2 = new Validacion("validacion 2","estamos", 2, validacionList);
+        public static Validacion validacion3 = new Validacion("validacion 3","listos", 3, validacionList);
         public static List<Validacion> listaValidacion = new List<Validacion>() { validacion1, validacion2, validacion3 };
 
         //Creamos informacion del cliente
@@ -53,12 +53,12 @@ namespace TPIDSI
         public static Cliente cliente1 = new Cliente(45690486, "Hassan", 3512012116, listaInformacion);
 
         //Creacion SubOpciones
-        public static SubOpcionLlamada subOpcion1 = new SubOpcionLlamada("SubOpcion 1", 1,listaValidacion);
-        public static SubOpcionLlamada subOpcion2 = new SubOpcionLlamada("SubOpcion 2", 2, listaValidacion);
+        public static SubOpcionLlamada subOpcion1 = new SubOpcionLlamada("Atencion al cliente", 1,listaValidacion);
+        public static SubOpcionLlamada subOpcion2 = new SubOpcionLlamada("subOpcion 2", 2, listaValidacion);
         public static List<SubOpcionLlamada> subOpciones = new List<SubOpcionLlamada>() { subOpcion1, subOpcion2 };
 
         //Creacion opcionLlamada
-        public static OpcionLlamada opcionLlamada = new OpcionLlamada("Hola", "como estas", "Camila", 1, listaValidacion, subOpciones);
+        public static OpcionLlamada opcionLlamada = new OpcionLlamada("Hola", "como estas", "Anular tarjeta", 1, listaValidacion, subOpciones);
         public static List<OpcionLlamada> opcionesLLamada = new List<OpcionLlamada>() { opcionLlamada };
 
 
@@ -66,7 +66,7 @@ namespace TPIDSI
         public static Llamada llamadaIniciada = new Llamada("Descripcion de opcion", "Detalle de Accion", 0, true, "nada", cliente1, accion1, opcionLlamada, subOpcion1, cambiosEstados);
 
         //Creacion categoria
-        public static CategoriaLlamada categoriaLlamada = new CategoriaLlamada("audio mensaje opciones", "mensaje opciones", "nombre", 1, opcionesLLamada);
+        public static CategoriaLlamada categoriaLlamada = new CategoriaLlamada("audio mensaje opciones", "mensaje opciones", "Informar Robo", 1, opcionesLLamada);
 
         
     }
